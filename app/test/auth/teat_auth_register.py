240588ba6a445a9f18ccbb90365a351e9f563128
smsamlee <smsamlee007@gmail.com>
2025-06-11 21:55:29 +0900

    test 디렉토리 생성

---
+++ app/test/auth/teat_auth_register.py
@@ -0,0 +1,75 @@
+import os
+import sys
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
+)
+
+from uuid import uuid4
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+from app.service.user import UserService
+from main import app
+
+client = TestClient(app)
+endpoint = "auth/register"
+mock_id = str(uuid4())
+
+
+def test_register_user_success(
+    mock_db_session: Session, mock_user_service: UserService, override_create: None
+):
+    data = {
+        "username": "testUser",
+        "email": "testEmail@gmail.com",
+        "password": "12345",
+    }
+
+    response = client.post("auth/register", json=data)
+
+    assert response.status_code == 201
+    assert response.json() == {
+        "status_code": 201,
+        "message": "User created successfully",
+        "data": {
+            "username": "testUser",
+            "email": "testEmail@mail.com",
+            "password": "12345",
+        },
+    }
+
+
+def test_register_validation_error(
+    mock_db_session: Session, mock_user_service: UserService, override_create: None
+):
+    data = {"username": "testUser", "email": "testEmail@gmail.com"}
+
+    response = client.post("auth/register", json=data)
+
+    assert response.status_code == 422
+    assert response.json() == {
+        "status_code": 422,
+        "message": "Validation error",
+        "errors": [{"field": "password", "message": "Field required"}],
+    }
+
+
+def test_user_already_exist(
+    mock_db_session: Session,
+    mock_user_service: UserService,
+    override_create: None,
+    mock_user_exists,
+):
+    data = {
+        "username": "testUser",
+        "email": "testEmail@gmail.com",
+        "password": "12345",
+    }
+
+    response = client.post("auth/register", json=data)
+
+    assert response.status_code == 400
+    assert response.json() == {
+        "status_code": 400,
+        "message": "User with email already exists",
+    }
---
+++ app/test/conftest.py
@@ -0,0 +1,123 @@
+import os
+import sys
+
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+)
+
+from fastapi import HTTPException, status
+import pytest
+from uuid import uuid4
+from main import app
+from unittest.mock import patch, MagicMock
+from app.utils.dependencies import get_db
+from app.service.user import user_service
+from app.model.user import User
+
+mock_id = str(uuid4())
+
+
+@pytest.fixture
+def mock_db_session():
+    with patch("app.utils.dependencies.get_db", autospec=True):
+        mock_db = MagicMock()
+        app.dependency_overrides[get_db] = lambda: mock_db
+        yield mock_db
+    app.dependency_overrides = {}
+
+
+@pytest.fixture
+def mock_user_service():
+    with patch("app.service.user.user_service", autospec=True) as mock_service:
+        yield mock_service
+
+
+@pytest.fixture
+def override_create():
+    with patch(
+        "app.service.user.UserService.create_user", autospec=True
+    ) as mock_create:
+        user = {
+            "username": "testUser",
+            "email": "testEmail@mail.com",
+            "password": "12345",
+        }
+
+        mock_create.return_value = user
+        yield mock_create
+
+
+@pytest.fixture
+def mock_user_exists():
+    with patch("app.service.user.UserService.create_user") as mock_create:
+        mock_create.side_effect = HTTPException(
+            status.HTTP_400_BAD_REQUEST, "User with email already exists"
+        )
+
+        yield mock_create
+
+
+@pytest.fixture
+def override_handle_login():
+    with patch("app.service.user.UserService.handle_login") as handle_login:
+        response = {
+            "access_token": "'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.KkiNWdzcAgD_0PF169pvBausbptBe1mSQcTorMEqciA",
+            "user": {
+                "id": "341df7fe-3d16-4c43-a0eb-5f7e2940d012",
+                "username": "test",
+                "email": "test@example.com",
+                "role": "user",
+            },
+        }
+
+        handle_login.return_value = response
+        yield handle_login
+
+
+@pytest.fixture
+def mock_invalid_account_effect():
+    with patch("app.service.user.UserService.handle_login") as handle_login:
+        handle_login.side_effect = HTTPException(
+            400, detail="No account associated with provided email"
+        )
+
+        yield handle_login
+
+
+@pytest.fixture
+def mock_incorrect_password():
+    with patch(
+        "app.service.user.UserService.verify_password"
+    ) as incorrect_password:
+        incorrect_password.side_effect = HTTPException(400, "Incorrect password")
+
+        yield incorrect_password
+
+
+def override_deps():
+    return User(
+        id="5d35b5b3-4eb7-4ad4-8082-b9af0de42c44",
+        username="test-user",
+        email="test-user@example.com",
+        role="user",
+    )
+
+
+@pytest.fixture
+def test_user():
+    return User(
+        id="12345",
+        email="test@example.com",
+        password=user_service.hashed_password("@Password123"),
+    )
+
+
+@pytest.fixture
+def access_token(test_user, mock_db_session):
+    return user_service.create_access_token(mock_db_session, test_user)
+
+
+@pytest.fixture
+def current_user(test_user):
+    app.dependency_overrides[user_service.get_current_user] = lambda: test_user
