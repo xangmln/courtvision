4c1d44b2fa808aaa710b93208eeb3e60ca7a7d68
smsamlee <smsamlee007@gmail.com>
2025-06-15 23:58:00 +0900

    court 스키마 수정

--- app/schemas/court.py
+++ app/schemas/court.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel, Field
-
+from typing import List
+from uuid import UUID
 
 class CourtBase(BaseModel):
     courtname : str = Field(min_length=1, max_length=20)
     location : str
 
 class CourtResponse(CourtBase):
-    timeslot : str
+    timeslot_id : List[UUID]
