7ca3d25e1c597b44adbfcd5eb83adb20f8df638a
smsamlee <smsamlee007@gmail.com>
2025-06-15 23:54:59 +0900

    코트 스키마 추가

--- app/schemas/court.py
+++ app/schemas/court.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel, Field
 
 
-class Court(BaseModel):
+class CourtBase(BaseModel):
     courtname : str = Field(min_length=1, max_length=20)
     location : str
+
+class CourtResponse(CourtBase):
+    timeslot : str
